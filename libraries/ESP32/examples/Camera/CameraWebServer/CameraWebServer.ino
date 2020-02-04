3a39f0e68664faff7477b60960b676b3ae7c83e8
Romain <romain@funtronic.co.uk>
2020-02-04 17:04:50 +0100

    The right configuration to get the ESP32CAM from M5STACK working! http://forum.m5stack.com/topic/992/please-help-has-anybody-had-success-with-the-esp32cam-on-arduino/3

--- libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino
+++ libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino
@@ -8,12 +8,12 @@
 //
 
 // Select camera model
-#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
 //#define CAMERA_MODEL_ESP_EYE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE	// Has PSRAM
 //#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
 #include "camera_pins.h"
 
